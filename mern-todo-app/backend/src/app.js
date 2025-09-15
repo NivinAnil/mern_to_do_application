303fd8c6ff50cff46135d7b8c3f5ec4628cbf5b0
Md-Talha-Rahmani <talhaprl3@gmail.com>
2025-09-15 20:39:06 +0530

    Urgent fix

--- mern-todo-app/backend/src/app.js
+++ mern-todo-app/backend/src/app.js
@@ -10,7 +10,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
